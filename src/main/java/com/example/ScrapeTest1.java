c2531bbc7401b33ac2e70aa75954b1ebccd6f9eb
Ken <kchessh@gmail.com>
2024-06-29 23:10:57 -0500

    First commit

--- src/main/java/com/example/ScrapeTest1.java
+++ src/main/java/com/example/ScrapeTest1.java
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class ScrapeTest1 extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ScrapeTest2 config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ScrapeTest2 provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ScrapeTest2.class);
 	}
 }
